9b66a86254d2ae1b94e04ecb5dec57e302e5abe0
Thiago Amaral <amaral.thi@live.com>
2024-08-04 15:46:26 -0300

    confirmaçãos de devolução e contagem de jogos alugados

--- js/app.js
+++ js/app.js
@@ -1,19 +1,35 @@
+let jogosAlugados= 0;
+
 function alterarStatus(id) {
     let jogo = document.getElementById(`game-${id}`);
     let imagem = jogo.querySelector('.dashboard__item__img');
     let botao = jogo.querySelector('.dashboard__item__button');
+    let nomeJogo = jogo.querySelector('.dashboard__item__name');
 
     if (imagem.classList.contains('dashboard__item__img--rented')) {
-        imagem.classList.remove('dashboard__item__img--rented');
-        botao.classList.remove('dashboard__item__button--return');
-        
-        botao.textContent = 'Alugar';
+        if (confirm(`Você tem certeza que deseja devolver o jogo ${nomeJogo.textContent}?`)) {
+            imagem.classList.remove('dashboard__item__img--rented');
+            botao.classList.remove('dashboard__item__button--return');
+
+            botao.textContent = 'Alugar';
+            jogosAlugados--;
+        }
         
     } else {
         imagem.classList.add('dashboard__item__img--rented');
         botao.classList.add('dashboard__item__button--return');
         
         botao.textContent = 'Devolver';
-        
+        jogosAlugados++;
     }
+    contarJogosAlugados();
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    jogosAlugados = document.querySelectorAll('.dashboard__item__img--rented').length;
+    contarEExibirJogosAlugados();
+});
+
+function contarJogosAlugados() {
+    alert(`Total de jogos alugados: ${jogosAlugados}`);
 }
